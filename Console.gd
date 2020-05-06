dc56b1e99c345f8d01c2ddb14ddbf12416ce3109
Chris <c.curteman@gmail.com>
2020-05-05 22:34:02 -0500

    Finished Interface Clean-up and Minor Code Refactoring

--- Console.gd
+++ Console.gd
@@ -1,24 +1,65 @@
 extends Control
 # Prototype for 4X style boardgame mechanics
 # Everything is thrown into this one big file
-# If development is to move forward past Monday, May 4th, 2020, refactoring will take place
 # The game is played in turns, thus there is no need to call physics_process or process
 
 
-# Exported Variables
-export var wood_to_lumber_ratio := 10
+# Resource Piles
+export var crude_oil_pile := 100
+export var copper_ore_pile := 100
+export var cotton_pile := 100
+export var fat_pile := 100
+export var flour_pile := 100
+export var food_pile := 100
+export var gold_pile := 100
+export var grain_pile := 100
+export var iron_ore_pile := 100
+export var lead_ore_pile := 100
+export var leather_pile := 100
+export var lumber_pile := 100
+export var meat_pile := 100
+export var ore_pile := 100
+export var pickaxe_pile := 100
+export var plow_pile := 100
+export var stone_pile := 100
+export var tin_ore_pile := 100
+export var total_population := 100
+export var woodaxe_pile := 100
 export var wood_pile := 1000
-export var lumber_pile := 0
-export var workers := 10
-export var carpenters := 0
+
+# Tiles
+export var exploited_forests := 0
+export var exploited_mines := 0
+export var exploited_farms := 0
+export var exploited_oil_fields := 0
+
+# Worker Management
+export var workers := 100
+export var lumberjacks := 100
+export var carpenters := 100
+var idle_workers_array : Array
+var idle_carpenters_array : Array
+var idle_lumberjacks_array : Array
+
+# Crafting
+export var wood_to_lumber_ratio := 10
+
+# Time
 export var worker_to_carpenter_train_time := 1
-export var lumberjacks := 0
 export var worker_to_lumberjacks_train_time := 1
 export var start_year := 0
-export var exploited_forests := 0
-
-# Private Variables
+export var max_years_allowed := 30
+export var seasons_list : Dictionary = {
+	0: "Spring",
+	1: "Summer",
+	2: "Fall",
+	3: "Winter"
+}
+var current_season_key := 0
 var current_year := 1
+var turn_counter := 0
+
+# Interface Only
 var actions_array : Array
 
 # Ready Dependent Variables
@@ -27,7 +68,7 @@ onready var temp_workers = workers
 
 
 
-# ---  BUILT-IN FUNCTIONS  --- #
+#####----- BUILT-IN FUNCTIONS -----#####
 func _ready():
 	Check_Actions()
 	Begin_Turn()
@@ -38,11 +79,10 @@ func _init() -> void:
 
 
 
-# ---  CUSTOM FUNCTIONS  --- #
+#####-----  CUSTOM FUNCTIONS -----#####
 # Takes in a string and prints it to the log. The action parameter will be converted to type string before being logged.
 func Log_Action(action):
 	$"Log/Log Text Line".insert_text_at_cursor(str(action) + "\n")
-
 
 # Checks every action in the game to see validate if said action.
 func Check_Actions():
@@ -59,17 +99,38 @@ func Add_Action(action):
 	# Log the order
 	Log_Action(str(action))
 
-
 # This function handles updating the interface, usually after Execute_Actions has been called.
 func Begin_Turn():
-	# Update Interface with new counts
-	$"Year Counter".text = "Year " + str(current_year)
-	# $"Item List".add_item("Test Order")
-	$"Wood".text = "Wood " + str(wood_pile)
-	$"Lumber".text = "Lumber " + str(lumber_pile)
-	$"Workers".text = "Workers " + str(workers)
-	$"Carpenters".text = "Carpenters " + str(carpenters)
+	# First we check if there are any win conditions or lose conditions that have been met
+	if current_year > max_years_allowed:
+		$"Temp Win Popup".visible = true
 
+	# If the player hasn't won, the turn begins
+	else:
+		# We want to change to the next season on a new turn
+		# But first we need to check if the season key is 3
+		# If it is, then we are going to start back at 0 and increment the year, else we increment it by 1
+		if current_season_key == 3:
+			current_season_key = 0
+			current_year += 1
+		else:
+			current_season_key += 1
+
+		# Next are aim is going to shift to checking what resources we are exploiting
+		if exploited_forests > 0:
+			wood_pile += exploited_forests * 100
+
+		# We need to update the turn counter to keep track of how many turns have past
+		turn_counter += 1
+
+		# Now we are going to update every element on the interface that tracks information per turn
+		$"Turn Counter".text = "Turn " + str(turn_counter)
+		$"Year Counter".text = "Year " + str(current_year)
+		$"Season Counter".text = seasons_list.get(current_season_key)
+		$"Wood".text = "Wood " + str(wood_pile)
+		$"Lumber".text = "Lumber " + str(lumber_pile)
+		$"Workers".text = "Workers " + str(workers)
+		$"Carpenters".text = "Carpenters " + str(carpenters)
 
 # Execute_Actions takes the action_array and uses it to execute the actions
 func Execute_Actions():
@@ -80,21 +141,6 @@ func Execute_Actions():
 
 		# The name of the order will be the name of the function, we use call() to execute the function of that action
 		call(action)
-
-
-# This function is usually called by Execute_Actions. It takes in a parameter, wood, and converts it into lumber at wood_to_lumber_ratio
-func Make_Lumber(wood):
-	var total_lumber_made = round(wood / wood_to_lumber_ratio)
-	wood_pile -= wood
-	lumber_pile += total_lumber_made
-
-
-# This function is usually called by Execute_Actions. It takes a parameter, total_workers, and
-func Train_Carpenters(total_workers):
-	# Validate that there are workers to convert
-	if total_workers < workers:
-		workers -= total_workers
-
 
 # Restart_Game will reset all the piles, exploited tiles, log contents and year to their default values and start the turn.
 func Restart_Game():
@@ -107,48 +153,86 @@ func Restart_Game():
 	$"Item List".clear()
 	Begin_Turn()
 
-	
+# Factory pattern based crafting function. This function takes items, ratio, and number of workers to craft what is being passed to it.
+func Craft_Factory(item_being_used, item_being_made, ratio, required_number_of_workers):
+	print(item_being_used, ratio, required_number_of_workers)
+	match item_being_made:
+		"Lumber":
+			Log_Action("Crafting Lumber")
 
-# ---  SIGNAL CONNECTIONS  --- #
+
+
+
+#####----- SIGNAL CONNECTIONS -----#####
 func _on_End_Turn_pressed():
-	print("Turn Ended")
-	current_year += 1
 	Execute_Actions()
 	Begin_Turn()
 	$"Item List".clear()
 
+	Log_Action("Turn " + str(turn_counter) + " has ended.")
 
+# This function is called when an order is selected in the item list
 func _on_Item_List_item_selected(_index):
-	$"Up".disabled = false
-	$"Down".disabled = false
-	$"Delete".disabled = false
+	$"Remove".disabled = false
+
+	Log_Action("A item was selected on the item list")
+
+# --- Worker Management --- #
+func _on_Train_Carpenter():
+	if workers >= 1:
+		workers -= 1
+		carpenters += 1
+	$"Carpenters".text = "Carpenters " + str(carpenters)
+	$"Workers".text = "Workers " + str(workers)
+
+	Log_Action("A Carpenter was trained.")
+
+func _on_Train_Lumberjack():
+	# We want players to instantly change worker values
+	# But first we check there is atleast one worker
+	if workers >= 1:
+		workers -= 1
+		lumberjacks += 1
+	$"Lumberjacks".text = "Lumberjacks " + str(lumberjacks)
+	$"Workers".text = "Workers " + str(workers)
+
+	Log_Action("A Lumberjack was trained.")
+
+func _on_Demote_Lumberjack() -> void:
+	if lumberjacks >= 1:
+		workers += 1
+		lumberjacks -= 1
+	$"Lumberjacks".text = "Lumberjacks " + str(lumberjacks)
+	$"Workers".text = "Workers " + str(workers)
+	Log_Action("A Lumberjack was demoted.")
+
+func _on_Demote_Carpenter() -> void:
+	if carpenters >= 1:
+		workers += 1
+		carpenters -= 1
+	$"Carpenters".text = "Carpenters " + str(carpenters)
+	$"Workers".text = "Workers " + str(workers)
+
+	Log_Action("A carpenter was demoted to worker")
 
 
-func _on_Convert_Worker__Carpenter_pressed():
-	# Checking if there are any available workers
-	if workers > 0:
-		# Adding Actions to the Queue
-		Add_Action("train_carpenters")
-
-		# Removing 1 workers for 1 carpenter and updating console
-		temp_workers -= 1
-		$"Workers".text = "Workers " + str(workers)
-
-		# Adding 1 carpenter for 1 worker and updating console
-		temp_carpenters += 1
-		$"Carpenters".text = "Carpenters " + str(carpenters)
-
-
-func _on_Convert_Worker_to_Lumberjack_pressed():
-	# We want to queue the order into the Add_Action so it will be executed on turn
-	Add_Action("Make_Lumberjack")
-
-
+# --- Game State --- #
 func _on_Restart_pressed() -> void:
 	$"Restart Confirmation".visible = true
-
 
 func _on_Restart_Confirmation_confirmed() -> void:
 	Restart_Game()
 
+func _on_Remove_Selected_Order() -> void:
+	var selected_items = $"Item List".get_selected_items()
+	for order_key in selected_items:
+		$"Item List".remove_item(order_key)
 
+	Log_Action("Order to remove an action from the order queue was made.")
+
+# This function handles all the logic related to exploiting a forest tile
+func _on_Exploit_Forest_pressed() -> void:
+	exploited_forests += 1
+	$"Item List".add_item("Start Forest Exploit")
+	$"Available Resources/Forest".text = "Forests - " + str(exploited_forests)
+	Log_Action("Order to start exploiting a forest was made.")
